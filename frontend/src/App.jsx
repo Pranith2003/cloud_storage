b44a9c8833f9153e314a179c8835f79c5bca2bba
sanjud1603 <sanjudinesh169@gmail.com>
2024-11-25 16:59:08 +0530

    Feat : added file sin frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Signin from "./components/auth/signin";
 import Signup from "./components/auth/signup";
 import Dashboard from "./components/dashboard";
-import FileUpload from "./components/file/FileUpload"
+import StorageMetrics from "./components/metrics/metrics";
+
 function App() {
   return (
     <Router>
@@ -10,11 +11,10 @@ function App() {
         <Route path="/signin" element={<Signin />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/file/FileUpload" element={<FileUpload />} />
+        <Route path="/metrics" element={<StorageMetrics />} />
       </Routes>
     </Router>
   );
 }
 
 export default App;
-//hello
